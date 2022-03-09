1f875f4d508e47ca2f08a7c2c5162a089213ef50
MinzPuiz <101197167+MinzPuiz@users.noreply.github.com>
2022-03-09 08:03:31 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ngọc Nhi';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100021737886423';
+const eventName = 'Happy Women"s Day';
+const titleCard = 'Dear bro :)))';
+const contentCard = 'Úm ba la alaba trap !!! Nói chứ hqua tui chúc lộn múi giờ nên nay chúc lại @@ Chúc cô Phương với Nhi có ngày 8/3 thật vui và hạnh phúc bên cạnh những người mà mình yêu thương :D Chúc cho những ước mơ, hoài bão của Nhi và mẹ sẽ sớm được hoàn thành. Có dịp về VN làm cái kèo nha pà ^3^ Cho ba mẹ tui với tui gửi lời hỏi thăm sức khỏe cô Phương với Tom nhé. Best wishes for you ^^ ';
+
+const giftImage = 'Nhi.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
